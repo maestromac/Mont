f20e46c806a8d0ee9f8d858e7152d76d01aa5d3a
Mac Siri <krairit.siri@gmail.com>
2016-08-13 18:05:34 -0400

    fix erb issue

--- lib/controller_base.rb
+++ lib/controller_base.rb
@@ -23,8 +23,7 @@ class ControllerBase
 
       res['location'] = url
       res.status = 302
-      session
-
+      session.store_session(res)
       @already_built_response = true
     else
       raise "Error!"
@@ -38,8 +37,8 @@ class ControllerBase
     unless already_built_response?
       res['content-type'] = content_type
       res.body = [content]
+      session.store_session(res)
       @already_built_response = true
-      session
     else
       raise "Error!"
     end
@@ -51,13 +50,13 @@ class ControllerBase
     controller_name = self.class.to_s
     path = "views/#{controller_name.underscore}/#{template_name}.html.erb"
     contents = File.read(path)
-    render_content( ERB.new("<%= contents %>").result(binding) , 'text/html')
+    erb = ERB.new(contents).result(binding)
+    render_content( erb , 'text/html')
   end
 
   # method exposing a `Session` object
   def session
     @session ||= Session.new(req)
-    @session.store_session(res)
   end
 
   # use this with the router to call action_name (:index, :show, :create...)
