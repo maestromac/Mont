f4c4d3529ea551da5f4f661c60b850c9eef384c5
Mac Siri <krairit.siri@gmail.com>
2016-10-05 15:20:47 -0400

    update CLI and naming

--- lib/mont.rb
+++ lib/mont.rb
@@ -1,7 +1,22 @@
-require "mont/version"
-require "cli"
-require "mont/my_mont"
+require 'mont/version'
+require 'thor'
+require 'mont/my_mont'
 
 module Mont
-  # Refer to cli.rb
+  class MyCLI < Thor
+    desc "new", "'new' will generate a new Mont application in your working directory.'"
+
+    def new(app_name)
+      g = Generator.new
+      g.directory "../new_app_files", "./#{app_name.chomp}"
+    end
+  end
+end
+
+class Generator < Thor
+  include Thor::Actions
+
+  def self.source_root
+    File.dirname(__FILE__)
+  end
 end
